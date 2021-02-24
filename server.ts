a1f10884b2c7f0d858f9750a2ccbd17f066f0491
maria.pabich.500@guest.p.lodz.pl <marysia.pabich@gmail.com>
2021-02-24 21:21:42 +0100

    initialize express server, db connection

---
+++ server.ts
@@ -0,0 +1,36 @@
+import express = require('express');
+import cors = require('cors');
+import path = require('path');
+import mongoose = require('mongoose');
+
+const app = express();
+
+/* MIDDLEWARE */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* API ENDPOINTS */
+
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+/* MONGOOSE */
+process.env.NODE_ENV === "production" ?
+  mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0-314sb.mongodb.net/EatnLounge?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }) :
+  mongoose.connect('mongodb://localhost:27017/eat-n-lounge', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Successfully connected to the database');
+});
+db.on('error', err => console.log('Error: ' + err));
+
+/* START SERVER */
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => {
+  console.log('Server is running on port: ' + port);
+});
+
+module.exports = server;
