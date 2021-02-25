74349c7c6e0c68906e77e84339a3a1ba39866128
maria.pabich.500@guest.p.lodz.pl <marysia.pabich@gmail.com>
2021-02-25 18:18:19 +0100

    add booking model

---
+++ src/models/booking.model.ts
@@ -0,0 +1,17 @@
+import mongoose = require('mongoose');
+import { DBBooking } from '../types';
+
+const bookingSchema = new mongoose.Schema({
+  surname: { type: String, required: true },
+  date: { type: String, required: true },
+  table: { type: String, required: true },
+  starters: { type: Array },
+  hours: { type: Number, required: true },
+  duration: { type: Number, required: true },
+  people: { type: Number, required: true },
+  phone: { type: Array, required: true },
+},
+  { versionKey: false }
+);
+
+export default mongoose.model<DBBooking>('Booking', bookingSchema);
