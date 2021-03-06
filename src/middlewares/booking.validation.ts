57636d4ded39cf02d7644619d9e8b2448408f758
maria.pabich.500@guest.p.lodz.pl <marysia.pabich@gmail.com>
2021-03-06 22:39:07 +0100

    return apropriate error code on table selection validation

--- src/middlewares/booking.validation.ts
+++ src/middlewares/booking.validation.ts
@@ -1,19 +1,17 @@
 import { errorCodes } from "../settings/codes";
 import { DBBooking, ValidateRequestParams } from "../types";
 
-
-
 class BookingValidation {
-	private tables = ['table_1', 'table_2', 'table_3', 'table_4'];
-	private invalidSigns = /[-[\]{}()*+?.,\\^$|#\s]/g;
-	private validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+	// private tables = ['table_1', 'table_2', 'table_3', 'table_4'];
+	// private invalidSigns = /[-[\]{}()*+?.,\\^$|#\s]/g;
+	// private validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 	
 	public validateBookingFields: ValidateRequestParams = (req, res, next) => { 
-	const { date, table, hours, people, phone, surname } = req.body as DBBooking;
+	const { /* date,  */table/* , hours, people, phone, surname  */} = req.body as DBBooking;
 	let isBookingValid = true;
 	if (!table) {
 		isBookingValid = false;
-		res.status(400).json({ error: true, errorCode: errorCodes.VALIDATION_ERROR_BAD_DURATION })
+		res.status(400).json({ error: true, errorCode: errorCodes.VALIDATION_ERROR_NO_TABLE_SELECTED })
 	}
 	if (isBookingValid) {
 		next();
